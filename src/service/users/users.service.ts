011a489ddfa8d44dd720e6a4a1645ba97016abc1
Brian Etaghene <joveee05@gmail.com>
2023-04-22 02:46:15 +0100

    [Added] CRUD operation for userModel

--- src/service/users/users.service.ts
+++ src/service/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/modules/users/users.model';
@@ -23,5 +23,50 @@ export class UsersService {
     });
     const response = await newUser.save({ validateBeforeSave: true });
     return response;
+  }
+  async getAllUsers() {
+    const users = await this.userModel.find().sort('-createdAt');
+    if (users.length < 1) {
+      throw new NotFoundException('No users found in database');
+    }
+    return users;
+  }
+
+  async getSingleUser(userId: string) {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException('Could not find user with this id');
+    }
+    return user;
+  }
+
+  async updateUser(
+    userId: string,
+    fullName: string,
+    userName: string,
+    email: string,
+    bio: string,
+  ) {
+    const user = await this.userModel.findByIdAndUpdate(userId);
+    if (!user) {
+      throw new NotFoundException('Could not find user with this id');
+    }
+    if (fullName) {
+      user.fullName = fullName;
+    } else if (userName) {
+      user.userName = userName;
+    } else if (email) {
+      user.email = email;
+    } else if (bio) {
+      user.bio = bio;
+    }
+    const result = await user.save({ validateBeforeSave: true });
+    return result;
+  }
+  async deleteUser(userId: string) {
+    const result = await this.userModel.findByIdAndDelete(userId);
+    if (!result) {
+      throw new NotFoundException('Could not find user with this id');
+    }
   }
 }
