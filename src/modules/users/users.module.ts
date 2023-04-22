217b60973ed0c14d098d90559a3ae8a193c24f9a
Brian Etaghene <joveee05@gmail.com>
2023-04-22 02:48:52 +0100

    Imported UsersController

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from 'src/service/users/users.service';
-// import { UsersController } from './users.controller';
+import { UsersController } from './users.controller';
 import { UserSchema } from './users.model';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  //   controllers: [UsersController],
+  controllers: [UsersController],
   providers: [UsersService],
 })
 export class UsersModule {}
