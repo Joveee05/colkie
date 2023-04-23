76080854cdae13500a20533e081fb5e7533d65f7
Brian Etaghene <joveee05@gmail.com>
2023-04-23 16:23:50 +0100

    [Added] Send message to a room

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Message } from './messages.model';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectModel('Message') private readonly messageModel: Model<Message>,
+  ) {}
+
+  async createMessage(body: string, roomID: string, userID: string) {
+    const newMessage = new this.messageModel({
+      body,
+      roomID,
+      userID,
+    });
+    const response = await newMessage.save({ validateBeforeSave: true });
+    return {
+      status: 'success',
+      message: 'Message created successfully',
+      data: response,
+    };
+  }
+}
