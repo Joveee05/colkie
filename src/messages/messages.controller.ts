3dff7343142bd36a08aedab84144f4c305d68666
Brian Etaghene <joveee05@gmail.com>
2023-04-24 02:30:30 +0100

    [Added] Get message by id

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  constructor(private messageService: MessagesService) {}
+
+  @Get(':id')
+  async getMessage(@Param('id') messageId: string) {
+    const message = await this.messageService.findMessage(messageId);
+    return {
+      status: 'success',
+      message: 'Message found',
+      data: message,
+    };
+  }
+}
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Message } from './messages.model';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectModel('Message') private readonly messageModel: Model<Message>,
+  ) {}
+  async findMessage(messageId: string) {
+    const message = await this.messageModel.findById(messageId);
+    if (!message) {
+      throw new NotFoundException('Could not find a message with this id');
+    }
+    return message;
+  }
+}
