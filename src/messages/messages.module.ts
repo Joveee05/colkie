36e280f51b624fd8bf313e9afb8b50ff2d493792
Brian Etaghene <joveee05@gmail.com>
2023-04-25 17:46:31 +0100

    [Added] send message to a room

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
+import { MessageSchema } from './messages.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RoomSchema } from 'src/rooms/rooms.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: 'Message',
+        useFactory: () => {
+          const schema = MessageSchema;
+          schema.pre(/^find/, function (next) {
+            this.populate({
+              path: 'roomID userID',
+              select: 'roomName fullName userName',
+            });
+            next();
+          });
+          return schema;
+        },
+      },
+    ]),
+    MongooseModule.forFeature([{ name: 'Room', schema: RoomSchema }]),
+  ],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Message } from './messages.model';
+import { Room } from 'src/rooms/rooms.model';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectModel('Message') private readonly messageModel: Model<Message>,
+    @InjectModel('Room') private readonly roomModel: Model<Room>,
+  ) {}
+
+  async pushToArray(roomId: string, messageId: string) {
+    await this.roomModel.findByIdAndUpdate(roomId, {
+      $push: { messages: messageId },
+    });
+  }
+
+  async numOfMessages(roomId: string) {
+    await this.roomModel.findByIdAndUpdate(roomId, {
+      $inc: { numOfMessages: 1 },
+    });
+  }
+  async createMessage(body: string, roomID: string, userID: string) {
+    const newMessage = new this.messageModel({
+      body,
+      roomID,
+      userID,
+    });
+    const response = await newMessage.save({ validateBeforeSave: true });
+    const messageId = response.id;
+    await this.pushToArray(roomID, messageId);
+    await this.numOfMessages(roomID);
+    return {
+      status: 'success',
+      message: 'Message created successfully',
+      data: response,
+    };
+  }
+}
