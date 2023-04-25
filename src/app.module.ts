be8d749e47dea14831b5052f7cff48c4199616a7
Brian Etaghene <joveee05@gmail.com>
2023-04-25 15:25:47 +0100

    [Deleted] rooms and messages modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { RoomsModule } from './rooms/rooms.module';
-import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
@@ -20,8 +18,6 @@ import { MessagesModule } from './messages/messages.module';
       }),
     }),
     AuthModule,
-    RoomsModule,
-    MessagesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
