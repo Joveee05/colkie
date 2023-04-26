babe2dd5ee0170f75288b5b5b741be694a005e76
Brian Etaghene <joveee05@gmail.com>
2023-04-26 22:49:35 +0100

    [Added] Swagger Docs Config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
@@ -8,6 +9,15 @@ async function bootstrap() {
   app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Colkie API')
+    .setDescription('The Colkie Rooms Managing API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(5000);
 }
 bootstrap();
