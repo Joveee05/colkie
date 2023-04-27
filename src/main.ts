4759bb7acff768cf527e7563d3e5126eee41270d
Brian Etaghene <joveee05@gmail.com>
2023-04-27 11:32:36 +0100

    [Added] Dockerfile and docker-compose

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
-  await app.listen(5000);
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
